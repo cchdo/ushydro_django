b3b1cf0e22ca754f2df30591252f6f65d5648a9a
Andrew Barna <abarna@gmail.com>
2013-08-09 16:01:39 -0700

    Remove hydrotable app from urls
    the hydrotable app is now hooked into django-cms

--- ushydro/urls.py
+++ ushydro/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
         # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
         # Uncomment the next line to enable the admin:
-        url(r'^hydrotable/', include('hydrotable.urls')),
+       # url(r'^hydrotable/', include('hydrotable.urls')),
         url(r'^admin/', include(admin.site.urls)),
         url(r'^', include('cms.urls')),
         )
