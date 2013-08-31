0523d35e95ad5a1c8af8c46b67e8123032b35fba
Andrew Barna <abarna@gmail.com>
2013-08-30 17:14:48 -0700

    fix PI names not displaying, start work on cruise modal

--- hydrotable/views.py
+++ hydrotable/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 
-from hydrotable.models import Cruise, Parameter
+from hydrotable.models import Cruise, Parameter, Program
 
 #@cache_page(60 * 15)
 def index(request):
@@ -10,8 +10,15 @@ def index(request):
     'chief_scientist', 'ship',)
 
     parameters = Parameter.objects.all()
+    programs = Program.objects.all().prefetch_related('pi')
+
+    pi_names = {}
+    for program in programs:
+        pi_names[program.id] = program.pi.name
+
     context = {'cruises': cruises,
-                'parameters': parameters
+                'parameters': parameters,
+                'pi_names': pi_names,
                 }
     return render(request, 'hydrotable/index.html', context)
 
