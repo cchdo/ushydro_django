ef317996dc565536da1d24fa732680baca6e231d
Andrew Barna <abarna@gmail.com>
2013-09-09 12:41:22 -0700

    make scrolling somewhat work on touch devices

--- ushydro/static/js/main.js
+++ ushydro/static/js/main.js
@@ -1,23 +1,33 @@
-
+var nav_top = 0;
 $(function(){
-    $(window).load(function() {
-        $('.navbar').affix({offset: $('.navbar').offset().top});
+  $(window).load(function() {
+    nav_top = $('.navbar').offset().top;
+    console.log(nav_top);
 
-        $('.sticker').css({'visibility':'hidden'});
-        $(this).scroll(function(){
-            if( $('.affix').css('position') == 'fixed' ) {
-              $('body').css({'padding-top':'70px'})
-                s = $('.sticker')
-                if ( s.css('visibility') == 'hidden'){
-                  s.css({'visibility':'visible'});
-                  s.css({'display':'none'});
-                };
-                s.fadeIn('fast');
+    $('.sticker').css({'visibility':'hidden'});
 
-            } else {
-                $('.sticker').css({'visibility':'hidden'});
-              $('body').css({'padding-top':'0px'})
-			}
-        });
-    });
+    $(this).bind("touchmove", scrollDeligate);
+    $(this).scroll(scrollDeligate);
+  });
 });
+function scrollDeligate(e){
+  console.log("called")
+  if ($(window).scrollTop() > nav_top) {
+    $('.navbar').css('position', 'fixed');
+  } else{
+    $('.navbar').css('position', 'static');
+  }
+  if( $('.navbar').css('position') == 'fixed' ) {
+    $('body').css({'padding-top':'70px'})
+      s = $('.sticker')
+      if ( s.css('visibility') == 'hidden'){
+        s.css({'visibility':'visible'});
+        s.css({'display':'none'});
+      };
+    s.fadeIn('fast');
+
+  } else {
+    $('.sticker').css({'visibility':'hidden'});
+    $('body').css({'padding-top':'0px'})
+  }
+}
