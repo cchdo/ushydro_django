eb542ad2e9a695cee924d2d82078cb7fa5e6c74b
Andrew Barna <abarna@gmail.com>
2014-01-23 12:23:11 -0800

    switch bibliography backend to zotero

--- bibliography/views.py
+++ bibliography/views.py
@@ -3,77 +3,34 @@ import os
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from pyzotero import zotero
+
 from bibliography.models import Bibliography
-from mendeley_client import create_client
 
 def _get_json():
+    zot = zotero.Zotero('220378' , 'group', 'xQkSPMDEBIeIjqgJsJlN8m0A')
+    zot.add_parameters(itemType="-attachment")
 
-    groupId = 3511431
-    config = os.path.join(os.path.dirname(__file__), 'config.json')
-    mendeley = create_client(config_file=config)
-    
-    response = mendeley.public_group_docs(groupId, items=1)
-    response = mendeley.public_group_docs(groupId, items=response['total_results'])
-    all_documents = []
-    for i, doc_id in enumerate(response['document_ids']):
-        response = mendeley.group_doc_details(groupId, doc_id)
-        all_documents.append(response)
+    items = zot.everything(zot.top())
+    itemlist = []
 
-    def sort(first, second=None, reverse=False):
-        """Sort group documents.
-
-        
-
-        """
-        def get_year(doc):
-            return doc.get('year', None)
-        def get_author(doc):
-            try:
-                #return doc['authors'][0]['surname']
-                return doc.get
-            except KeyError, IndexError:
-                return None
-
-        def get_attribute(doc, key):
-            if key == 'year':
-                return get_year(doc)
-            elif key == 'author':
-                return get_author(doc)
-            else:
-                return None
-
-        def get_key(doc):
-            key = []
-            key.append(get_attribute(doc, first))
-            if second:
-                key.append(get_attribute(doc, second))
-            return tuple(key)
-
-        return sorted(all_documents, key=get_key, reverse=reverse)
-    
-    sorted_doc=sort('year', 'author')
-
-    #test = []
-    #for xxx in year_author_sort:
-    #    test.append([xxx['year'], xxx['authors'][0]['surname'], xxx['mendeley_url']])
-    docs = []
-
-    for doc in sorted_doc:
-        docs.append({
-            'label': doc['title'],
-            #'type': "Document",
-            'year':     doc['year'],
-            'authors':  '; '.join([', '.join([elem.get('surname', None), elem.get('forename', None)]) for elem in doc['authors']]),
-            'uri':      doc['mendeley_url'],
-            'abstract': ' '.join(doc.get('abstract', '').split(' ')[:100]) + '...',
-            'publisher' : doc.get('publication_outlet', ''),
-        })
+    for unit in items:
+        itemlist.append({
+            'label': unit.get('title', ''),
+            'year': unit.get('date', ''),
+            'authors': '; '.join(
+                [', '.join([elem.get('lastName', None),
+                elem.get('firstName', None)]) for elem in\
+                unit.get('creators', {})]),
+            'uri': unit.get('url', ''),
+            'publisher': unit.get('publicationTitle', ''),
+        })         
 
     exhibit_json = {
-        'types': {
-           "Document": {
-               'pluralLabel':  "Documents",
-           },
+            'types': {
+            "Document": {
+                'pluralLabel':  "Documents",
+            },
         },
         'properties': {
             #'year' : {
@@ -84,12 +41,13 @@ def _get_json():
             #},
             #'publisher' : {
             #    'valueType':    "string"
-            #},
+                #},
         },
-        'items': docs,
+        'items': itemlist,
     }
 
     return json.dumps(exhibit_json)
+
 
 def index(request):
     return render(request, "bibliography/index.html")
@@ -108,4 +66,4 @@ def load_bibliography(request):
 
     b.text = _get_json()
     b.save()
-    return HttpResponse('it worked load.json')
+    return HttpResponse('Bibliography updated succesfully')
